47a9b86ce918aedc4e5a2f42d096fc2c4600de2a
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2022-03-18 17:18:35 -0300

    add files

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
+import Layout from '@components/Layout/Layout'
+import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
+import ProductList from '@components/ProductList/ProductList'
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/avo')
+  const {data: productList}: TAPIAvoResponse = await response.json()
+
+  return {
+    props: {
+      productList,
+    }
+  }
+}
+
+const HomePage = ({ productList } : {productList: TProduct[]}) => {
+
+  return (
+    <Layout>
+      <KawaiiHeader />
+      <ProductList products={productList} />
+    </Layout>
+  )
+}
+
+export default HomePage
